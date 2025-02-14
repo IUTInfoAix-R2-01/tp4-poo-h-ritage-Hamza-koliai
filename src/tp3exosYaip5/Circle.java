f691cccab7e87720446af63934b5ccf59b38a6f8
koliaih <hamza.koliai@etu.univ-amu.fr>
2025-02-14 11:00:59 +0100

    Yaip5 1er version

---
+++ src/tp3exosYaip5/Circle.java
@@ -0,0 +1,32 @@
+package tp3exosYaip5;
+
+public class Circle {
+    double radius;
+    String color;
+
+    public Circle() {
+        radius = 1.0;
+        color = "red";
+    }
+
+    public Circle(double r) {
+        radius = r;
+        color = "red";
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+    
+    public static void main (String []args) {
+    	Circle c = new Circle();
+    	System.out.println(c.getRadius());
+    	System.out.println(c.getArea());
+    }
+    
+    
+}
---
+++ src/tp3exosYaip5/Cylindre.java
@@ -0,0 +1,35 @@
+package tp3exosYaip5;
+
+public class Cylindre {
+    private Circle base;   
+    private double height;
+
+    public Cylindre(double radius, double height) {
+        this.base = new Circle(radius);
+        this.height = height;
+    }
+
+    public double getVolume() {
+        return base.getArea() * height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getBaseArea() {
+        return base.getArea();
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Cylindre cylindre1 = new Cylindre(5.0, 10.0);
+
+        System.out.println("Rayon du cylindre : " + cylindre1.base.getRadius());
+        System.out.println("Aire du cylindre : " + cylindre1.getBaseArea());
+        System.out.println("Hauteur du cylindre : " + cylindre1.getHeight());
+        System.out.println("Volume du cylindre : " + cylindre1.getVolume());
+    }
+}
